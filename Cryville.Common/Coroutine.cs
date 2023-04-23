dbaed75a6c151a3edad0443a058fdc06d5695b87
PopSlime <popslime@qq.com>
2023-04-24 00:41:22 +0800

    Add `Coroutine`.

---
+++ Cryville.Common/Coroutine.cs
@@ -0,0 +1,26 @@
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace Cryville.Common {
+	public class Coroutine {
+		readonly IEnumerator<float> _enumerator;
+		readonly Stopwatch _stopwatch = new Stopwatch();
+		public float Progress { get; private set; }
+		public Coroutine(IEnumerator<float> enumerator) {
+			_enumerator = enumerator;
+		}
+		public bool TickOnce() {
+			if (!_enumerator.MoveNext()) return false;
+			Progress = _enumerator.Current;
+			return true;
+		}
+		public bool Tick(double minTime) {
+			_stopwatch.Restart();
+			while (_stopwatch.Elapsed.TotalSeconds < minTime) {
+				if (!_enumerator.MoveNext()) return false;
+				Progress = _enumerator.Current;
+			}
+			return true;
+		}
+	}
+}
