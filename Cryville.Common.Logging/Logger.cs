ff43774aba6191422b7f0ad64d00136aca2cfd03
PopSlime <popslime@qq.com>
2023-11-20 16:39:14 +0800

    Rewrite Cryville.Common.Logging.

--- Cryville.Common.Logging/Logger.cs
+++ Cryville.Common.Logging/Logger.cs
@@ -1,126 +1,96 @@
 using System;
 using System.Collections.Generic;
-using System.Globalization;
-using System.IO;
 
 namespace Cryville.Common.Logging {
 	/// <summary>
 	/// A logger.
 	/// </summary>
-	public abstract class Logger {
-		static readonly Dictionary<string, Logger> Instances = new Dictionary<string, Logger>();
-		static readonly Dictionary<string, StreamWriter> Files = new Dictionary<string, StreamWriter>();
-		static string logPath = null;
+	public class Logger {
+		readonly HashSet<LoggerListener> _listeners = [];
+
 		/// <summary>
-		/// Sets the path where the log files shall be stored.
+		/// Attaches a listener to the logger.
 		/// </summary>
-		/// <param name="path">The path.</param>
-		public static void SetLogPath(string path) {
-			logPath = path;
-			var dir = new DirectoryInfo(path);
-			if (!dir.Exists) dir.Create();
-		}
-		/// <summary>
-		/// Logs to the specified logger.
-		/// </summary>
-		/// <param name="key">The key of the logger.</param>
-		/// <param name="level">The severity level.</param>
-		/// <param name="module">The module that is logging.</param>
-		/// <param name="format">The format string.</param>
-		/// <param name="args">The arguments for formatting.</param>
-		public static void Log(string key, int level, string module, string format, params object[] args) {
-			if (!Instances.TryGetValue(key, out var logger)) return;
-			logger.Log(level, module, string.Format(format, args));
-			if (Files.ContainsKey(key)) Files[key].WriteLine("[{0:O}] [{1}] <{2}> {3}", DateTime.UtcNow, level, module, string.Format(format, args));
-		}
-		/// <summary>
-		/// Adds a created logger to the shared logger manager.
-		/// </summary>
-		/// <param name="key">The key of the logger.</param>
-		/// <param name="logger">The logger.</param>
-		public static void Create(string key, Logger logger) {
-			Instances[key] = logger;
-			if (logPath != null) {
-				Files[key] = new StreamWriter(logPath + "/" + ((int)DateTime.UtcNow.Subtract(new DateTime(1970, 1, 1)).TotalSeconds).ToString(CultureInfo.InvariantCulture) + "-" + key + ".log") {
-					AutoFlush = true
-				};
+		/// <param name="listener">The logger listener.</param>
+		public void AddListener(LoggerListener listener) {
+			lock (_listeners) {
+				_listeners.Add(listener);
 			}
 		}
 		/// <summary>
-		/// Closes all loggers and related file streams.
+		/// Detaches a listener from the logger.
 		/// </summary>
-		public static void Close() {
-			Instances.Clear();
-			foreach (var f in Files) f.Value.Dispose();
-			Files.Clear();
+		/// <param name="listener">The logger listener.</param>
+		public void RemoveListener(LoggerListener listener) {
+			lock (_listeners) {
+				_listeners.Remove(listener);
+			}
+		}
+
+		/// <summary>
+		/// Logs to the logger.
+		/// </summary>
+		/// <param name="level">The severity level.</param>
+		/// <param name="category">The category.</param>
+		/// <param name="format">The format string.</param>
+		/// <param name="args">The arguments for formatting.</param>
+		public void Log(int level, string category, string format, params object[] args)
+			=> Log(level, category, null, format, args);
+		/// <summary>
+		/// Logs to the logger.
+		/// </summary>
+		/// <param name="level">The severity level.</param>
+		/// <param name="category">The category.</param>
+		/// <param name="provider">The format provider.</param>
+		/// <param name="format">The format string.</param>
+		/// <param name="args">The arguments for formatting.</param>
+		public void Log(int level, string category, IFormatProvider? provider, string format, params object[] args)
+			=> Log(level, category, string.Format(provider, format, args));
+		/// <summary>
+		/// Logs to the logger.
+		/// </summary>
+		/// <param name="level">The severity level.</param>
+		/// <param name="category">The category.</param>
+		/// <param name="message">The message.</param>
+		public void Log(int level, string category, string message) {
+			lock (_listeners) {
+				foreach (var listener in _listeners)
+					listener.OnLog(level, category, message);
+			}
 		}
 		/// <summary>
 		/// Logs to the logger.
 		/// </summary>
 		/// <param name="level">The severity level.</param>
-		/// <param name="module">The module that is logging.</param>
-		/// <param name="msg">The message.</param>
-		public virtual void Log(int level, string module, string msg) { }
-	}
-
-	/// <summary>
-	/// A <see cref="Logger" /> that calls a callback function on log.
-	/// </summary>
-	public class InstantLogger : Logger {
-		readonly Action<int, string, string> callback;
+		/// <param name="category">The category.</param>
+		/// <param name="message">An array of <see cref="char" /> containing the message.</param>
+		public void Log(int level, string category, char[] message) => Log(level, category, message ?? throw new ArgumentNullException(nameof(message)), 0, message.Length);
 		/// <summary>
-		/// Creates an instance of the <see cref="InstantLogger" /> class.
+		/// Logs to the logger.
 		/// </summary>
-		/// <param name="callback">The callback function.</param>
-		/// <exception cref="ArgumentNullException"><paramref name="callback" /> is <see langword="null" />.</exception>
-		public InstantLogger(Action<int, string, string> callback) {
-			this.callback = callback ?? throw new ArgumentNullException("callback");
-		}
-		/// <inheritdoc />
-		public override void Log(int level, string module, string msg) {
-			base.Log(level, module, msg);
-			callback(level, module, msg);
-		}
-	}
-
-	/// <summary>
-	/// A <see cref="Logger" /> that buffers the logs for enumeration.
-	/// </summary>
-	public class BufferedLogger : Logger {
-		readonly List<LogEntry> buffer = new List<LogEntry>();
-		/// <summary>
-		/// Creates an instance of the <see cref="BufferedLogger" /> class.
-		/// </summary>
-		public BufferedLogger() { }
-		/// <inheritdoc />
-		public override void Log(int level, string module, string msg) {
-			base.Log(level, module, msg);
-			lock (buffer) {
-				buffer.Add(new LogEntry(level, module, msg));
+		/// <param name="level">The severity level.</param>
+		/// <param name="category">The category.</param>
+		/// <param name="message">An array of <see cref="char" /> containing the message.</param>
+		/// <param name="index">A zero-based index of the first character of the message within <paramref name="message" />.</param>
+		/// <param name="length">The length of the message.</param>
+		public void Log(int level, string category, char[] message, int index, int length) {
+			lock (_listeners) {
+				foreach (var listener in _listeners)
+					listener.OnLog(level, category, message, index, length);
 			}
 		}
 		/// <summary>
-		/// Enumerates the buffered logs.
+		/// Logs to the logger.
 		/// </summary>
-		/// <param name="callback">The callback function to receive the logs.</param>
-		public void Enumerate(Action<int, string, string> callback) {
-			lock (buffer) {
-				foreach (var i in buffer) {
-					callback(i.level, i.module, i.msg);
-				}
+		/// <param name="level">The severity level.</param>
+		/// <param name="category">The category.</param>
+		/// <param name="message">A pointer to the first character of the message.</param>
+		/// <param name="length">The length of the message.</param>
+		public unsafe void Log(int level, string category, char* message, int length) {
+			lock (_listeners) {
+				foreach (var listener in _listeners)
+					listener.OnLog(level, category, message, length);
 			}
-			buffer.Clear();
-		}
-	}
-
-	struct LogEntry {
-		public int level;
-		public string module;
-		public string msg;
-		public LogEntry(int level, string module, string msg) {
-			this.level = level;
-			this.module = module;
-			this.msg = msg;
 		}
 	}
 }
---
+++ Cryville.Common.Logging/LoggerListener.cs
@@ -0,0 +1,124 @@
+using System;
+using System.Collections.Concurrent;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Text;
+
+namespace Cryville.Common.Logging {
+	/// <summary>
+	/// A logger listener.
+	/// </summary>
+	public abstract class LoggerListener : IDisposable {
+		/// <summary>
+		/// Closes the logger listener and cleans up all the resources.
+		/// </summary>
+		/// <param name="disposing">Whether to clean up managed resources.</param>
+		protected virtual void Dispose(bool disposing) { }
+		/// <summary>
+		/// Closes the logger listener.
+		/// </summary>
+		public void Dispose() {
+			Dispose(true);
+			GC.SuppressFinalize(this);
+		}
+
+		/// <summary>
+		/// Handles an incoming log.
+		/// </summary>
+		/// <param name="level">The severity level.</param>
+		/// <param name="category">The category.</param>
+		/// <param name="message">The message.</param>
+		protected internal abstract void OnLog(int level, string category, string message);
+		/// <summary>
+		/// Handles an incoming log.
+		/// </summary>
+		/// <param name="level">The severity level.</param>
+		/// <param name="category">The category.</param>
+		/// <param name="message">An array of <see cref="char" /> containing the message.</param>
+		/// <param name="index">A zero-based index of the first character of the message within <paramref name="message" />.</param>
+		/// <param name="length">The length of the message.</param>
+		protected internal virtual unsafe void OnLog(int level, string category, char[] message, int index, int length) {
+			fixed (char* ptr = message) {
+				OnLog(level, category, ptr + index, length);
+			}
+		}
+		/// <summary>
+		/// Handles an incoming log.
+		/// </summary>
+		/// <param name="level">The severity level.</param>
+		/// <param name="category">The category.</param>
+		/// <param name="message">A pointer to the first character of the message.</param>
+		/// <param name="length">The length of the message.</param>
+		protected internal virtual unsafe void OnLog(int level, string category, char* message, int length)
+			=> OnLog(level, category, new string(message, 0, length));
+	}
+
+	/// <summary>
+	/// A <see cref="LoggerListener" /> that calls a callback function on log.
+	/// </summary>
+	public class InstantLoggerListener : LoggerListener {
+		/// <summary>
+		/// Occurs when a log is logged to the logger.
+		/// </summary>
+		[SuppressMessage("Design", "CA1003")]
+		public event LogHandler? Log;
+
+		/// <inheritdoc />
+		protected internal override void OnLog(int level, string category, string message) {
+			Log?.Invoke(level, category, message);
+		}
+	}
+
+	/// <summary>
+	/// A <see cref="LoggerListener" /> that buffers the logs for enumeration.
+	/// </summary>
+	public class BufferedLoggerListener : LoggerListener {
+		readonly ConcurrentQueue<LogEntry> buffer = new();
+		record struct LogEntry(int Level, string Category, string Message);
+
+		/// <inheritdoc />
+		protected internal override void OnLog(int level, string category, string message) {
+			buffer.Enqueue(new LogEntry(level, category, message));
+		}
+		/// <summary>
+		/// Enumerates the buffered logs.
+		/// </summary>
+		/// <param name="callback">The callback function to receive the logs.</param>
+		public void Enumerate(LogHandler callback) {
+			while (buffer.TryDequeue(out var i)) {
+				callback?.Invoke(i.Level, i.Category, i.Message);
+			}
+		}
+	}
+
+	/// <summary>
+	/// A <see cref="LoggerListener" /> that writes logs into a stream.
+	/// </summary>
+	/// <param name="stream">The stream.</param>
+	/// <param name="encoding">The encoding.</param>
+	public class StreamLoggerListener(Stream stream, Encoding encoding) : LoggerListener {
+		/// <summary>
+		/// Creates an instance of the <see cref="StreamLoggerListener" /> class.
+		/// </summary>
+		/// <param name="stream">The stream.</param>
+		public StreamLoggerListener(Stream stream) : this(stream, new UTF8Encoding(false, true)) { }
+
+		readonly object _syncRoot = new();
+		/// <inheritdoc />
+		protected internal override void OnLog(int level, string category, string message) {
+			lock (_syncRoot) {
+				var buf = encoding.GetBytes(string.Format(CultureInfo.InvariantCulture, "[{0:O}] [{1}] <{2}> {3}", DateTime.UtcNow, level, category, message));
+				stream.Write(buf, 0, buf.Length);
+			}
+		}
+	}
+
+	/// <summary>
+	/// Represents the method that will handle a log.
+	/// </summary>
+	/// <param name="level">The severity level.</param>
+	/// <param name="category">The category.</param>
+	/// <param name="message">The message.</param>
+	public delegate void LogHandler(int level, string category, string message);
+}
