e3dbc591ffa7933b76b78cdac6ee663ce17be386
PopSlime <popslime@qq.com>
2024-03-09 12:29:20 +0800

    Add parameterized constructor to `MonoPInvokeCallbackAttribute` for Xamarin support.

--- Cryville.Common.Interop/MonoPInvokeCallbackAttribute.cs
+++ Cryville.Common.Interop/MonoPInvokeCallbackAttribute.cs
@@ -5,5 +5,21 @@ namespace Cryville.Common.Interop {
 	/// Attribute used to annotate functions that will be called back from the unmanaged world.
 	/// </summary>
 	[AttributeUsage(AttributeTargets.Method)]
-	public sealed class MonoPInvokeCallbackAttribute : Attribute { }
+	public sealed class MonoPInvokeCallbackAttribute : Attribute {
+		/// <summary>
+		/// The type of the delegate that will be calling us back.
+		/// </summary>
+		public Type? DelegateType { get; }
+
+		/// <summary>
+		/// Constructor for the MonoPInvokeCallbackAttribute.
+		/// </summary>
+		[Obsolete("Use the parameterized constructor for Xamarin support.")]
+		public MonoPInvokeCallbackAttribute() { }
+		/// <summary>
+		/// Constructor for the MonoPInvokeCallbackAttribute.
+		/// </summary>
+		/// <param name="delegateType">The type of the delegate that will be calling us back.</param>
+		public MonoPInvokeCallbackAttribute(Type delegateType) { DelegateType = delegateType; }
+	}
 }
