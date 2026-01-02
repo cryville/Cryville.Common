9a61502e62a226e18e9c068c4f4c080bb6d6eada
PopSlime <popslime@qq.com>
2026-01-02 18:16:07 +0800

    feat(compat): Update throw helper

---
+++ Cryville.Common.Compat/EnumValues.cs
@@ -0,0 +1,13 @@
+using System.Runtime.CompilerServices;
+
+namespace Cryville.Common.Compat {
+	/// <summary>
+	/// Provides enum constants for backward compatibility.
+	/// </summary>
+	public static class EnumValues {
+		/// <summary>
+		/// <see cref="MethodImplOptions" />.AggressiveInlining
+		/// </summary>
+		public const MethodImplOptions MethodImplOptionsAggressiveInlining = (MethodImplOptions)256;
+	}
+}
--- Cryville.Common.Compat/ThrowHelper.cs
+++ Cryville.Common.Compat/ThrowHelper.cs
@@ -1,10 +1,8 @@
 using System;
-#if NETSTANDARD2_1_OR_GREATER || NETCOREAPP3_0_OR_GREATER
+using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
-#endif
-#if NETCOREAPP3_0_OR_GREATER
+using System.Globalization;
 using System.Runtime.CompilerServices;
-#endif
 
 namespace Cryville.Common.Compat {
 	/// <summary>
@@ -17,16 +15,10 @@ namespace Cryville.Common.Compat {
 		/// <param name="argument">The reference type argument to validate as non-null.</param>
 		/// <param name="paramName">The name of the parameter with which <paramref name="argument" /> corresponds. If you omit this parameter, the name of <paramref name="argument" /> is used.</param>
 		/// <exception cref="ArgumentNullException"><paramref name="argument" /> is <see langword="null" />.</exception>
-		public static void ThrowIfNull(
-#if NETSTANDARD2_1_OR_GREATER || NETCOREAPP3_0_OR_GREATER
-			[NotNull]
+#if NET6_0_OR_GREATER
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
 #endif
-			object? argument,
-#if NETCOREAPP3_0_OR_GREATER
-			[CallerArgumentExpression("argument")]
-#endif
-			string? paramName = null
-		) {
+		public static void ThrowIfNull([NotNull] object? argument, [CallerArgumentExpression(nameof(argument))] string? paramName = null) {
 #if NET6_0_OR_GREATER
 			ArgumentNullException.ThrowIfNull(argument, paramName);
 #else
@@ -41,22 +33,20 @@ namespace Cryville.Common.Compat {
 		/// <param name="paramName">The name of the parameter with which <paramref name="argument" /> corresponds.</param>
 		/// <exception cref="ArgumentNullException"><paramref name="argument" /> is <see langword="null" />.</exception>
 		/// <exception cref="ArgumentException"><paramref name="argument" /> is empty.</exception>
-		public static void ThrowIfNullOrEmpty(
-#if NETSTANDARD2_1_OR_GREATER || NETCOREAPP3_0_OR_GREATER
-			[NotNull]
+#if NET7_0_OR_GREATER
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
 #endif
-			string? argument,
-#if NETCOREAPP3_0_OR_GREATER
-			[CallerArgumentExpression("argument")]
-#endif
-			string? paramName = null
-		) {
+		public static void ThrowIfNullOrEmpty([NotNull] string? argument, [CallerArgumentExpression(nameof(argument))] string? paramName = null) {
 #if NET7_0_OR_GREATER
 			ArgumentException.ThrowIfNullOrEmpty(argument, paramName);
 #else
-			if (string.IsNullOrEmpty(argument)) {
+			if (IsNullOrEmpty(argument)) {
 				ThrowIfNull(argument, paramName);
-				throw new ArgumentException("Empty string.", paramName);
+				throw new ArgumentException("The value cannot be an empty string.", paramName);
+			}
+			[MethodImpl(EnumValues.MethodImplOptionsAggressiveInlining)]
+			static bool IsNullOrEmpty([NotNullWhen(false)] string? value) {
+				return string.IsNullOrEmpty(value);
 			}
 #endif
 		}
@@ -68,40 +58,173 @@ namespace Cryville.Common.Compat {
 		/// <param name="paramName">The name of the parameter with which <paramref name="argument" /> corresponds.</param>
 		/// <exception cref="ArgumentNullException"><paramref name="argument" /> is <see langword="null" />.</exception>
 		/// <exception cref="ArgumentException"><paramref name="argument" /> is empty or consists only of white-space characters.</exception>
-		public static void ThrowIfNullOrWhiteSpace(
-#if NETSTANDARD2_1_OR_GREATER || NETCOREAPP3_0_OR_GREATER
-			[NotNull]
+#if NET8_0_OR_GREATER
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
 #endif
-			string? argument,
-#if NETCOREAPP3_0_OR_GREATER
-			[CallerArgumentExpression("argument")]
-#endif
-			string? paramName = null
-		) {
+		public static void ThrowIfNullOrWhiteSpace([NotNull] string? argument, [CallerArgumentExpression(nameof(argument))] string? paramName = null) {
 #if NET8_0_OR_GREATER
 			ArgumentException.ThrowIfNullOrWhiteSpace(argument, paramName);
 #else
 			if (IsNullOrWhiteSpace(argument)) {
 				ThrowIfNull(argument, paramName);
-				throw new ArgumentException("Empty or white-space string.", paramName);
+				throw new ArgumentException("The value cannot be an empty string or composed entirely of whitespace.", paramName);
+			}
+			[MethodImpl(EnumValues.MethodImplOptionsAggressiveInlining)]
+			static bool IsNullOrWhiteSpace([NotNullWhen(false)] string? value) {
+#if NET40_OR_GREATER || NETSTANDARD1_0_OR_GREATER || NETCORE1_0_OR_GREATER
+				return string.IsNullOrWhiteSpace(value);
+#else
+				if (value == null) return true;
+				foreach (var c in value) {
+					if (!char.IsWhiteSpace(c)) return false;
+				}
+				return true;
+#endif
 			}
 #endif
 		}
 
-		static bool IsNullOrWhiteSpace(
-#if NETSTANDARD2_1_OR_GREATER || NETCOREAPP3_0_OR_GREATER
-			[NotNullWhen(false)]
+		/// <summary>
+		/// Throws an <see cref="ArgumentOutOfRangeException" /> if <paramref name="value" /> is equal to <paramref name="other" />.
+		/// </summary>
+		/// <typeparam name="T">The type of the objects to validate.</typeparam>
+		/// <param name="value">The argument to validate as not equal to <paramref name="other" />.</param>
+		/// <param name="other">The value to compare with <paramref name="value" />.</param>
+		/// <param name="paramName">The name of the parameter with which <paramref name="value" /> corresponds.</param>
+		/// <exception cref="ArgumentOutOfRangeException"><paramref name="value" /> is equal to <paramref name="other" />.</exception>
+#if NET8_0_OR_GREATER
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
 #endif
-			string? value
-		) {
-#if NET40_OR_GREATER || NETSTANDARD1_0_OR_GREATER || NETSTANDARD1_0_OR_GREATER
-			return string.IsNullOrWhiteSpace(value);
+		public static void ThrowIfEqual<T>(T value, T other, [CallerArgumentExpression(nameof(value))] string? paramName = null) where T : IEquatable<T>? {
+#if NET8_0_OR_GREATER
+			ArgumentOutOfRangeException.ThrowIfEqual(value, other, paramName);
 #else
-			if (value == null) return true;
-			foreach (var c in value) {
-				if (!char.IsWhiteSpace(c)) return false;
+			if (EqualityComparer<T>.Default.Equals(value, other)) {
+				throw new ArgumentOutOfRangeException(
+					paramName, value,
+					string.Format(CultureInfo.InvariantCulture, "{0} ('{1}') must not be equal to '{2}'.", paramName, (object?)value ?? "null", (object?)other ?? "null")
+				);
 			}
-			return true;
+#endif
+		}
+
+		/// <summary>
+		/// Throws an <see cref="ArgumentOutOfRangeException" /> if <paramref name="value" /> is not equal to <paramref name="other" />.
+		/// </summary>
+		/// <typeparam name="T">The type of the objects to validate.</typeparam>
+		/// <param name="value">The argument to validate as equal to <paramref name="other" />.</param>
+		/// <param name="other">The value to compare with <paramref name="value" />.</param>
+		/// <param name="paramName">The name of the parameter with which <paramref name="value" /> corresponds.</param>
+		/// <exception cref="ArgumentOutOfRangeException"><paramref name="value" /> is not equal to <paramref name="other" />.</exception>
+#if NET8_0_OR_GREATER
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+#endif
+		public static void ThrowIfNotEqual<T>(T value, T other, [CallerArgumentExpression(nameof(value))] string? paramName = null) where T : IEquatable<T>? {
+#if NET8_0_OR_GREATER
+			ArgumentOutOfRangeException.ThrowIfNotEqual(value, other, paramName);
+#else
+			if (!EqualityComparer<T>.Default.Equals(value, other)) {
+				throw new ArgumentOutOfRangeException(
+					paramName, value,
+					string.Format(CultureInfo.InvariantCulture, "{0} ('{1}') must be equal to '{2}'.", paramName, (object?)value ?? "null", (object?)other ?? "null")
+				);
+			}
+#endif
+		}
+
+		/// <summary>
+		/// Throws an <see cref="ArgumentOutOfRangeException" /> if <paramref name="value" /> is greater than <paramref name="other" />.
+		/// </summary>
+		/// <typeparam name="T">The type of the objects to validate.</typeparam>
+		/// <param name="value">The argument to validate as less than or equal to <paramref name="other" />.</param>
+		/// <param name="other">The value to compare with <paramref name="value" />.</param>
+		/// <param name="paramName">The name of the parameter with which <paramref name="value" /> corresponds.</param>
+		/// <exception cref="ArgumentOutOfRangeException"><paramref name="value" /> is greater than <paramref name="other" />.</exception>
+#if NET8_0_OR_GREATER
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+#endif
+		public static void ThrowIfGreaterThan<T>(T value, T other, [CallerArgumentExpression(nameof(value))] string? paramName = null) where T : IComparable<T> {
+#if NET8_0_OR_GREATER
+			ArgumentOutOfRangeException.ThrowIfGreaterThan(value, other, paramName);
+#else
+			if (value.CompareTo(other) > 0) {
+				throw new ArgumentOutOfRangeException(
+					paramName, value,
+					string.Format(CultureInfo.InvariantCulture, "{0} ('{1}') must be less than or equal to '{2}'.", paramName, (object?)value ?? "null", (object?)other ?? "null")
+				);
+			}
+#endif
+		}
+
+		/// <summary>
+		/// Throws an <see cref="ArgumentOutOfRangeException" /> if <paramref name="value" /> is greater than or equal to <paramref name="other" />.
+		/// </summary>
+		/// <typeparam name="T">The type of the objects to validate.</typeparam>
+		/// <param name="value">The argument to validate as less than <paramref name="other" />.</param>
+		/// <param name="other">The value to compare with <paramref name="value" />.</param>
+		/// <param name="paramName">The name of the parameter with which <paramref name="value" /> corresponds.</param>
+		/// <exception cref="ArgumentOutOfRangeException"><paramref name="value" /> is greater than or equal to <paramref name="other" />.</exception>
+#if NET8_0_OR_GREATER
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+#endif
+		public static void ThrowIfGreaterThanOrEqual<T>(T value, T other, [CallerArgumentExpression(nameof(value))] string? paramName = null) where T : IComparable<T> {
+#if NET8_0_OR_GREATER
+			ArgumentOutOfRangeException.ThrowIfGreaterThanOrEqual(value, other, paramName);
+#else
+			if (value.CompareTo(other) >= 0) {
+				throw new ArgumentOutOfRangeException(
+					paramName, value,
+					string.Format(CultureInfo.InvariantCulture, "{0} ('{1}') must be less than '{2}'.", paramName, (object?)value ?? "null", (object?)other ?? "null")
+				);
+			}
+#endif
+		}
+
+		/// <summary>
+		/// Throws an <see cref="ArgumentOutOfRangeException" /> if <paramref name="value" /> is less than <paramref name="other" />.
+		/// </summary>
+		/// <typeparam name="T">The type of the objects to validate.</typeparam>
+		/// <param name="value">The argument to validate as greater than or equal to <paramref name="other" />.</param>
+		/// <param name="other">The value to compare with <paramref name="value" />.</param>
+		/// <param name="paramName">The name of the parameter with which <paramref name="value" /> corresponds.</param>
+		/// <exception cref="ArgumentOutOfRangeException"><paramref name="value" /> is less than <paramref name="other" />.</exception>
+#if NET8_0_OR_GREATER
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+#endif
+		public static void ThrowIfLessThan<T>(T value, T other, [CallerArgumentExpression(nameof(value))] string? paramName = null) where T : IComparable<T> {
+#if NET8_0_OR_GREATER
+			ArgumentOutOfRangeException.ThrowIfLessThan(value, other, paramName);
+#else
+			if (value.CompareTo(other) < 0) {
+				throw new ArgumentOutOfRangeException(
+					paramName, value,
+					string.Format(CultureInfo.InvariantCulture, "{0} ('{1}') must be greater than or equal to '{2}'.", paramName, (object?)value ?? "null", (object?)other ?? "null")
+				);
+			}
+#endif
+		}
+
+		/// <summary>
+		/// Throws an <see cref="ArgumentOutOfRangeException" /> if <paramref name="value" /> is less than or equal to <paramref name="other" />.
+		/// </summary>
+		/// <typeparam name="T">The type of the objects to validate.</typeparam>
+		/// <param name="value">The argument to validate as greater than <paramref name="other" />.</param>
+		/// <param name="other">The value to compare with <paramref name="value" />.</param>
+		/// <param name="paramName">The name of the parameter with which <paramref name="value" /> corresponds.</param>
+		/// <exception cref="ArgumentOutOfRangeException"><paramref name="value" /> is less than or equal to <paramref name="other" />.</exception>
+#if NET8_0_OR_GREATER
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+#endif
+		public static void ThrowIfLessThanOrEqual<T>(T value, T other, [CallerArgumentExpression(nameof(value))] string? paramName = null) where T : IComparable<T> {
+#if NET8_0_OR_GREATER
+			ArgumentOutOfRangeException.ThrowIfLessThanOrEqual(value, other, paramName);
+#else
+			if (value.CompareTo(other) <= 0) {
+				throw new ArgumentOutOfRangeException(
+					paramName, value,
+					string.Format(CultureInfo.InvariantCulture, "{0} ('{1}') must be greater than '{2}'.", paramName, (object?)value ?? "null", (object?)other ?? "null")
+				);
+			}
 #endif
 		}
 	}
